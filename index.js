93840479922cfe9bcc073c1c6415bd2a9cb77f77
Joshua Alvarado <joshalva3@gmail.com>
2022-11-10 23:38:38 -0600

    more tidy code

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('commit for the day!');
+// console.log('commit for the day!');
+
+// ( <----- TIDY CODE EXAMPLES ----> DONE ON CODEPEN)
+
+// let favoriteActivity = 'coding';
+
+// describe('favoriteActivity', function() {
+
+//     it('should be the string coding', function() {
+//         expect(favoriteActivity).toEqual('coding');
+//     })
+// })
+
+function add() {
+    
+}
+
+
+describe('add', function() {
+    it(`should add two numbers together`, function() {
+        const result = add(1, 2);
+        expect(result).toEqual(3);
+    })
+})
+
