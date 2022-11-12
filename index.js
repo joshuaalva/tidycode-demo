aa20fa265ecfa872be32a5076964e8e61d6e926e
Joshua Alvarado <joshalva3@gmail.com>
2022-11-11 23:39:46 -0600

    more tidy code examples transferred from codepen

--- index.js
+++ index.js
@@ -1,25 +1,89 @@
 // console.log('commit for the day!');
 
-// ( <----- TIDY CODE EXAMPLES ----> DONE ON CODEPEN)
+// <-- TIDY CODE EXMAPLES --> 
 
-// let favoriteActivity = 'coding';
+// Style Me Pretty 
 
-// describe('favoriteActivity', function() {
+// function styleMePretty() {
+//     let truth = '';
+// function innerBeauty(count) {
+// let result = '';
+// while(count > 0) {
+//   count--;
+//     if(count===2) {
+//     result += 'Unformatted code';
+//     }
+// } result += ' is difficult to parse';
+// return result;} truth += innerBeauty(5); truth += ' for humans if not for machines';
+// return truth;
+// }
 
-//     it('should be the string coding', function() {
-//         expect(favoriteActivity).toEqual('coding');
-//     })
-// })
+// describe('styleMePretty', function() {
 
-function add() {
-    
-}
+// it('is a function', function() {
+//   expect(typeof styleMePretty).toEqual('function');
+// });
 
+// it('returns a string', function() {
+//   let returnedValue = styleMePretty();
+//   expect(typeof returnedValue).toEqual('string');
+// });
 
-describe('add', function() {
-    it(`should add two numbers together`, function() {
-        const result = add(1, 2);
-        expect(result).toEqual(3);
-    })
-})
+// it('returns the correct message', function() {
+//   let returnedValue = styleMePretty();
+//   expect(returnedValue).toEqual(
+//     'Unformatted code is difficult to parse for humans if not for machines'
+//   );
+// });
+
+// });
+
+// Just In Camel Case 
+
+// function justInCamelCase(undername) {
+//     let camelcaseoutput = "";
+//       let foundunder = false;
+//       for(let i = 0; i<undername.length; i++) {
+//       if (undername[i] === "_") {
+//         foundunder = true;
+//         } else {
+//           if (foundunder) {
+//           camelcaseoutput += undername[i].toUpperCase();
+//         foundunder = false;
+//       } else {
+//         camelcaseoutput += undername[i];
+//         }
+//     }
+//     }
+//         return camelcaseoutput;
+//   }
+  
+//   describe('justInCamelCase', function() {
+  
+//     it('is a function', function() {
+//       expect(typeof justInCamelCase).toEqual('function');
+//     });
+  
+//     it('returns a string', function() {
+//       let returnedValue = justInCamelCase('any string value');
+//       expect(typeof returnedValue).toEqual('string');
+//     });
+  
+//     it('returns a camelCased string given a string with one underscore', function() {
+//       let returnedValue = justInCamelCase('snake_case');
+//       expect(returnedValue).toEqual('snakeCase');
+//     });
+  
+//     it('returns a camelCased string given a string with two underscores', function() {
+//       let returnedValue = justInCamelCase('underscore_the_point');
+//       expect(returnedValue).toEqual('underscoreThePoint');
+//     });
+  
+//     it('returns the inputted string unchanged given a lower-case string with no underscores', function() {
+//       let returnedValue = justInCamelCase('lowercase');
+//       expect(returnedValue).toEqual('lowercase');
+//     });
+  
+//   });
+  
 
