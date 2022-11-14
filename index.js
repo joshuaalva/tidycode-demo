49a00507dffd94756234b731d96e800f4b26a952
Joshua Alvarado <joshalva3@gmail.com>
2022-11-13 23:33:09 -0600

    ceaser cypher

--- index.js
+++ index.js
@@ -85,5 +85,36 @@
 //     });
   
 //   });
+
+// Ceasar Cypher 
+
+// describe('Caesar Cypher', () => {
+
+//     it('is a function?', () => {
+//       expect(typeof caesarCypher).toEqual('function');
+//     });
+  
+//     it('rotates a letter by the number passed in', () => {
+//       let returnedValue = caesarCypher('a', 2);
+//       expect(returnedValue).toEqual('c');
+//     });
+  
+//     it('rotates every letter in the string by the supplied letter', () => {
+//       let returnedValue = caesarCypher('doggo', 7);
+//       expect(returnedValue).toEqual('kvnnv');
+//     });
+  
+//     it('wraps around to the beginning of the alphabet when necessary', () => {
+//       let returnedValue = caesarCypher('hello', 13);
+//       expect(returnedValue).toEqual('uryyb');
+//     });
+  
+//     it('retains spaces between encrypter world', () => {
+//       let returnedValue = caesarCypher('hello world', 13);
+//       expect(returnedValue).toEqual('uryyb jbeyq');
+//     });
+  
+//   });
+  
   
 
