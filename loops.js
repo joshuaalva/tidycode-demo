cf8dffb5a85f5dcae7bdd30f66d6d68504195165
Joshua Alvarado <joshalva3@gmail.com>
2022-11-23 23:22:42 -0600

    continue statement recursion bonus loop

--- loops.js
+++ loops.js
@@ -11,42 +11,54 @@
 //     console.log(n); //expected output 3
 // }
 
-{
-  let n = 0;
+// {
+//   let n = 0;
 
-  while (n <= 3) {
-    n++;
-  }
+//   while (n <= 3) {
+//     n++;
+//   }
 
-  console.log(n);
-}
+//   console.log(n);
+// }
 
 // indebugger red dot on the left side is called a break point
 
 // do while loop checks in the end instead of teh beginning
 // not really neccesary 
 
-let result = '';
-let index = 0;
+// let result = '';
+// let index = 0;
 
-do {
-    index = index + 1;
-    result = result + index;
-} while (index < 5);
+// do {
+//     index = index + 1;
+//     result = result + index;
+// } while (index < 5);
 
-console.log(result);
+// console.log(result);
 
 // cstyle for loops 
 // for ([initialExpression]; [conditionExpression; [incrementExpression]])
 //statement 
 
-{
-let str = ''
-for (let i = 0; i < 10; i++) {
-    str = str + i;
-}
-console.log(str);
+// {
+// let str = ''
+// for (let i = 0; i < 10; i++) { // i = i + 2 even numbers
+//     str = str + i;
+// }
+// console.log(str);
 
+// }
+
+// continue statement
+
+let text = ``;
+
+for(let index = 0; index < 10; index++) {
+    if (index === 3) {
+        continue;
+    }
+
+    text = text + index;
 }
 
-// daily log
+console.log(text);
