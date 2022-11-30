ec2be871da82bdbfd4e3a4dcae4e64b08463b99a
Joshua Alvarado <joshalva3@gmail.com>
2022-11-30 12:43:52 -0600

    exponentiate problem v1

--- loops.js
+++ loops.js
@@ -207,5 +207,38 @@ function bacteriaTime(currentNum, targetNum) {
   
   // 04 Exponentiate
 
+  // YOUR CODE BELOW
+function exponentiate(base, power){
+    let result = 1;
+    
+    for(let i = 0; i < power; i++) {
+      result *= base;
+    }
+    
+    return result;
+  }
+  
+  describe('exponentiate', () => {
+  
+    it('is a function', () => {
+      expect(typeof exponentiate).toEqual('function');
+    });
+  
+    it('returns a number', () => {
+      let returnedValue = exponentiate(1, 1);
+      expect(typeof returnedValue).toEqual('number');
+    });
+  
+    it('returns the value of the first num raised to the second', () => {
+      let returnedValue = exponentiate(3, 6);
+      expect(returnedValue).toEqual(Math.pow(3, 6));
+    });
+  
+    it('returns 1 if the power is 0', () => {
+      let returnedValue = exponentiate(20, 0);
+      expect(returnedValue).toEqual(1);
+    });
+  
+  });
   
   
