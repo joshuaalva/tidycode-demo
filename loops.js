91e76da5c9de7748ce048af4df109752f858e952
Joshua Alvarado <joshalva3@gmail.com>
2022-11-30 12:27:42 -0600

    bacteria time function

--- loops.js
+++ loops.js
@@ -161,5 +161,51 @@ function crazyCaps(originalString) {
   
   });
 
-  // 03
+  // 03 Bacteria Time 
+
+  // YOUR CODE BELOW
+function bacteriaTime(currentNum, targetNum) {
+    if(targetNum < currentNum){
+      return `targetNum must be larger than currentNum`
+    }
+    
+    let numMinutes = 0; 
+    
+    while(currentNum < targetNum){
+      currentNum *= 2;
+      numMinutes += 20;
+    }
+    
+    return numMinutes;
+  }
+  
+  console.log(bacteriaTime(1, 8));
+  
+  describe('bacteriaTime', () => {
+  
+    it('is a function', () => {
+      expect(typeof bacteriaTime).toEqual('function');
+    });
+  
+    it('returns the number of minutes required to reach the target', () => {
+      let returnedValue = bacteriaTime(1, 8);
+      expect(returnedValue).toEqual(60);
+    });
+  
+    it('returns the number of minutes required to reach a large target', () => {
+      let returnedValue = bacteriaTime(3, 6000);
+      expect(returnedValue).toEqual(220);
+    });
+  
+    it('returns a string warning if the targetNum is invalid', () => {
+      let returnedValue = bacteriaTime(100, 50);
+      expect(returnedValue).toEqual('targetNum must be larger than currentNum');
+    });
+  
+  });
+
+  
+  // 04 Exponentiate
+
+  
   
