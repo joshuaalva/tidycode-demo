f7edea4cb2df67b910c21c51a1c2473f027cf03e
Joshua Alvarado <joshalva3@gmail.com>
2022-12-04 23:52:12 -0600

    my index of

--- loops.js
+++ loops.js
@@ -260,7 +260,6 @@ function exponentiate(base, power){
 
 // 05 My Slice 
 
-// YOUR CODE BELOW
 function mySlice(originalString, startIdx, endIdx) {
     if(startIdx === 'undefined' || endIdx === 'undefined') {
       return originalString;
@@ -309,6 +308,37 @@ function mySlice(originalString, startIdx, endIdx) {
     it('returns the original string if no indices are inputted', () => {
       let returnedValue = mySlice('a piece of pie');
       expect(returnedValue).toEqual('a piece of pie');
+    });
+  
+  });
+
+// 06 my index of
+
+
+describe('myIndexOf', () => {
+
+    it('is a function', () => {
+      expect(typeof myIndexOf).toEqual('function');
+    });
+  
+    it('returns a number', () => {
+      let returnedValue = myIndexOf('i love apples', 'apples');
+      expect(typeof returnedValue).toEqual('number');
+    });
+  
+    it('returns the first instance of the searchValue in the source', () => {
+      let returnedValue = myIndexOf('here and there', 'here');
+      expect(returnedValue).toEqual(0);
+    });
+  
+    it('returns the first instance of the searchValue at or after the startIdx', () => {
+      let returnedValue = myIndexOf('here and there', 'here', 4);
+      expect(returnedValue).toEqual(10);
+    });
+  
+    it('returns -1 if the searchValue is not in the source', () => {
+      let returnedValue = myIndexOf('here and there', 'nowhere');
+      expect(returnedValue).toEqual(-1);
     });
   
   });
